c99450b71dfe15483606c9c14ef3c72dab8d893b
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-12-22 23:33:55 +0000

    div ghost is working & being styled as requested

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const divAction = document.getElementById('poem');
+const divGhost = document.createElement('div');
 
 function createGhostDiv() {
-    const divGhost = document.createElement('div');
-    divGhost.textContent = 'I am ghost. I talk to you. But you cannot hear me. I shove you. But you cannot feel me. I look at you. But you cannot see me. I am invisible';
-    divGhost.style.display.block;
+    divGhost.innerHTML = 'I am ghost. I talk to you. But you cannot hear me. I shove you. But you cannot feel me. I look at you. But you cannot see me. I am invisible';
+    divGhost.style.display = 'block';
+    divGhost.style.width = '400px';
+    divGhost.style.position = 'absolute';
+    divGhost.style.left = '400px';
+    divGhost.style.color = 'rgba(156, 156, 154)';
     divAction.appendChild(divGhost);
 }
 
 function removeGhostDiv() {
-    const divGhostAgain = document.getElementById('i-am-ghost');
-    if (divGhostAgain) {
-        divGhostAgain.remove();
+    if (divGhost) {
+        divAction.removeChild(divGhost);
     }
 }
 
